525c09bf47a1b51de01c3a7a90bf22a58acf5d76
Marco Massaro <marco.massaro@studenti.unicam.it>
2026-04-09 12:51:01 +0200

    Refactor main method to enhance Pokedex loading and discovery process

--- app/src/main/java/mdp2026/pokedex/App.java
+++ app/src/main/java/mdp2026/pokedex/App.java
@@ -1,7 +1,25 @@
 package mdp2026.pokedex;
 
+import org.checkerframework.checker.units.qual.s;
+
+import mdp2026.pokedex.model.Pokedex;
+import mdp2026.pokedex.service.PokedexLoader;
+
 public class App {
     public static void main(String[] args) {
-        
+        try {
+            Pokedex pokedex = new PokedexLoader().caricaPokedex();
+            System.out.println("Pokedex caricato con successo!");
+            System.out.println(pokedex);
+            System.out.println("Scoperta del Pokemon con ID 78...");
+            pokedex.discoverPokemon(78);
+            for (int id = 1; id <= 76; id++) {
+                pokedex.discoverPokemon(id);
+            }
+            System.out.println("Pokemon scoperto!");
+            System.out.println(pokedex);
+        } catch (RuntimeException e) {
+            System.err.println("Errore fatale: " + e.getMessage());
+        }
     }
 }
